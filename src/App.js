ae49494649b866df51662de465cc185966292ab2
Langa Nhlapo <lsnhlapo@gmail.com>
2022-08-07 19:41:46 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Details from './components/Details'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Convergenc3 Tech Assessment</h1>
+      <Details />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Details.js
@@ -0,0 +1,9 @@
+const Details = () => {
+  return (
+    <div class="inputFields">
+      <h3>Input Fields</h3>
+    </div>
+  )
+}
+
+export default Details
